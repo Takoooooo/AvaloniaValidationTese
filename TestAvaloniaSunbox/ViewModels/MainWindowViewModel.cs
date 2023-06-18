ecc508f0e04204289d614524e9ca4166c08b8f99
Daniil Pavliuchyk <tako0qq@gmail.com>
2023-06-18 20:00:04 +0300

    Add project files.

---
+++ TestAvaloniaSunbox/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using ReactiveUI.Validation.Abstractions;
+using ReactiveUI.Validation.Contexts;
+using ReactiveUI.Validation.Extensions;
+using System;
+
+namespace TestAvaloniaSunbox.ViewModels;
+
+public class MainWindowViewModel : ReactiveObject, IValidatableViewModel
+{
+    [Reactive]
+    public int Value { get; set; }
+
+    public MainWindowViewModel()
+    {
+        IObservable<bool> valtest =
+            this.WhenAnyValue(
+                x => x.Value,
+                (v) => v != 0);
+
+        this.ValidationRule(
+            vm => vm.Value, // The property name selector expression.
+            valtest, // IObservable<bool>
+            "You should set NumericUpDown value!");
+    }
+
+    public ValidationContext ValidationContext { get; } = new ValidationContext();
+}
